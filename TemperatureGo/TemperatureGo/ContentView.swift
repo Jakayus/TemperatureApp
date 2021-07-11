cd8a911db55bf18b22d4b2ead7a4ee058dfbc744
Jakayus <kawika@mail.usf.edu>
2021-07-11 00:42:17 -0700

    SwiftUI Starter Project
    Initial Hello World project in SwiftUI

---
+++ TemperatureGo/TemperatureGo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TemperatureGo
+//
+//  Created by Joel Sereno on 7/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TemperatureGo/TemperatureGo/TemperatureGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureGoApp.swift
+//  TemperatureGo
+//
+//  Created by Joel Sereno on 7/11/21.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
