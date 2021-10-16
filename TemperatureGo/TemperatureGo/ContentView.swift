af6c2e0706fa298f8501b79ed4c3b60c7ea7fee4
Jakayus <kawika@mail.usf.edu>
2021-10-15 23:59:07 -0700

    Initial Layout design as well as target change to ios15

--- TemperatureGo/TemperatureGo/ContentView.swift
+++ TemperatureGo/TemperatureGo/ContentView.swift
@@ -9,34 +9,59 @@ import SwiftUI
 
 struct ContentView: View {
     
-    //MARK: PROPERTIES
+    //MARK: - PROPERTIES
+    @State private var tempInput = ""
+    let temperatureTypes = ["Fahrenheit", "Celsius"]
+   
+    @State private var selectedTemp = "Fahrenheit"
     
-    
-    
+    //MARK: - VIEW
     var body: some View {
         
         VStack {
-            Text("Temperature Go")
+            Text("Temperature Go!")
+                .font(.system(.title, design: .rounded))
+                .bold()
+                .padding()
+                .background(Color.blue)
+                .cornerRadius(5)
+           
                 //.padding()
             Text("Your personal Temperature converter")
-                .font(.caption2)
+                .font(.subheadline)
+                .foregroundColor(Color.secondary)
+            Spacer()
             
             HStack {
-                Text("Degrees")
-                Text("Type")
+                Text("Degrees:")
+                TextField("Temperature", text: $tempInput)
+            }
+            Picker("Choose a tempature baseline", selection: $selectedTemp) {
+                ForEach (temperatureTypes, id: \.self) {
+                    Text($0)
+                }
             }
             
             Button(action: {
                 
             }, label: {
                 Text("Convert")
+                    .font(.system(.title, design: .rounded))
+                    .foregroundColor(.black)
+                    .bold()
+                    .padding()
+                    .background(Color.green)
+                    .cornerRadius(4)
             })
-            
+            Spacer()
             
             Text("Result")
-            
+
         }
     }
+    
+    //MARK: - FUNCTIONS
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
